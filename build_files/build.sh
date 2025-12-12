d1d7dd9a1bec12a88515bba76212993cd888fcc2
skullbite <git@bork.quest>
2025-12-12 00:40:45 -0500

    init

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,7 +10,11 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y tmux 
+dnf5 -y install 'dnf5-command(copr)'
+# this installs a package from fedora repos
+dnf -y copr enable ryanabx/cosmic-epoch
+dnf -y install cosmic-desktop
+
 
 # Use a COPR Example:
 #
